6f3b9cfe61c7d6f415120f9bb632d6ece1b953f6
Fredrik Rubensson <fredrik@eldfluga.se>
2012-09-21 21:02:09 +0200

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merkado::Application.config.secret_token = '1054f997f8ee7b6f8f22d3ecaf1c034a568bce92581787a07f95151b67295e51d8667df60d53effbcb42b34642774b6d9da29c17a128b73d1c39c2f7966adb3f'
