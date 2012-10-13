c2af3f2ca4b121082ce8c4e4c50d5e7923cc6cd6
froderik <fredrik@eldfluga.se>
2012-10-13 18:39:25 +0200

    spec formatting

--- spec/integration/landing_page_spec.rb
+++ spec/integration/landing_page_spec.rb
@@ -3,23 +3,27 @@ require 'spec_helper'
 feature 'as a user I would like a landing page: ' do
   scenario 'visit root - find site name' do
     visit '/'
+
     page.should have_content 'merkado'
     page.should_not have_content 'Sign out'
   end
 
   scenario 'sign in with nonexisting user' do
     sign_in 'finns.inte@example.com', ''
+
     page.should have_content 'Invalid user'
   end
 
   scenario 'sign in with existing user' do
     create_and_sign_in_user
+
     page.should have_content 'Available order books'
   end
 
   scenario 'sign out' do
     create_and_sign_in_user
     page.should have_content 'Sign out'
+
     click_on 'Sign out'
     page.should_not have_content 'Sign out'
   end
@@ -27,7 +31,9 @@ feature 'as a user I would like a landing page: ' do
   scenario 'honor password' do
     email = 'a.user@exmple.com'
     create_user email, 'lousy'
+
     sign_in email, 'anotherpassword'
+
     page.should have_content 'Invalid user'
   end
 end
--- spec/integration/order_book_list_spec.rb
+++ spec/integration/order_book_list_spec.rb
@@ -4,7 +4,9 @@ feature 'as a user I want to see a list of all order books I care about' do
   scenario 'list order books I administer' do
     user = create_and_sign_in_user
     order_book = create_order_book_with_admin user
+
     visit '/order_books'
+
     page.should have_content order_book.name
   end
 
@@ -13,7 +15,9 @@ feature 'as a user I want to see a list of all order books I care about' do
     order_book      = create_order_book 'Sega gubbar'
     concurrent_book = create_order_book 'Sega gummor'
     add user, order_book
+
     visit '/order_books'
+
     page.should     have_content      order_book.name
     page.should_not have_content concurrent_book.name
   end
@@ -22,8 +26,10 @@ feature 'as a user I want to see a list of all order books I care about' do
     user            = create_and_sign_in_user
     order_book      = create_order_book 'Sega gubbar'
     add user, order_book
+
     visit '/order_books'
     click_on order_book.name
+
     page.current_url.should =~ Regexp.new( "order_books/#{order_book.id}" )
   end
 end
