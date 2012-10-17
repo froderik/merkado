fc25082424b85003ee250ca8ee6aaede3812b9ea
Petter Skiden <petter.skiden@gmail.com>
2012-10-17 18:37:28 +0200

    Internationalization part 1. Added locale to user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def default_url_options(options={})
+  	logger.debug "default_url_options is passed options: #{options.inspect}\n"
+  	{ :locale => I18n.locale }
+	end
+
+	before_filter :set_locale
+ 
+	def set_locale
+    if session[:user_id]
+      user = CouchPotato.database.load session[:user_id]
+  	  I18n.locale = user.locale || I18n.default_locale
+    else
+      I18n.locale = I18n.default_locale
+    end
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include CouchPotato::Persistence
 
   property :email
+  property :locale
 
   view :by_email, :key => :email
 end
