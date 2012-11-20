5ae134257082cc871138707fa7d57ca308af4276
froderik <fredrik@eldfluga.se>
2012-11-20 17:49:15 +0100

    a bit nicer looking

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,6 @@
 class TradesController < ApplicationController
   def index
+    @instrument = Couch.find_by_id params[:instrument_id]
     @trades = Trade.find_by_instrument_id params[:instrument_id]
   end
 end
