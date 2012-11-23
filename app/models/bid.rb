123f7d35228acbcb000651590c38d89a6595857e
froderik <fredrik@eldfluga.se>
2012-11-23 13:35:06 +0100

    added methods to find by instrument

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,2 +1,6 @@
 class Bid < Order
+  def self.find_by_instrument_id instrument_id
+    descending = true
+    super.find_by_instrument_id instrument_id, descending
+  end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,4 @@
 class Offer < Order
+  def self.find_by_instrument_id instrument_id
+    super.find_by_instrument_id instrument_id
 end
